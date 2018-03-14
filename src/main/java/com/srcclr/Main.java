c030209cf4e06e1b9445078635d77557867ce7b1
Darius Foo <darius@srcclr.com>
2018-03-14 18:32:13 +0800

    Minimal example

--- src/main/java/com/srcclr/Main.java
+++ src/main/java/com/srcclr/Main.java
@@ -1,7 +1,5 @@
 package com.srcclr;
 
-import org.apache.commons.fileupload.MultipartStream;
-import org.apache.xml.security.signature.XMLSignatureInput;
 import org.mindrot.jbcrypt.BCrypt;
 
 import java.io.ByteArrayInputStream;
@@ -14,18 +12,5 @@ public class Main {
     String hashed = BCrypt.hashpw(candidate, BCrypt.gensalt(12));
 
     BCrypt.checkpw(candidate, hashed);
-
-    filterXMLSignature();
-  }
-
-  private static void filterXMLSignature() {
-    byte[] bytes = new byte[256];
-
-    try {
-      new MultipartStream(new ByteArrayInputStream(bytes), bytes);
-    } catch (IOException ignored) {
-    }
-
-    new XMLSignatureInput(bytes).addNodeFilter(null);
   }
 }
