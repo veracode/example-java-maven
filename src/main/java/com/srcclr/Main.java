44a308c96404314432495596c95860a814424a41
Darius Foo <darius@srcclr.com>
2018-03-14 20:17:29 +0800

    Add another vulnerable method call

--- src/main/java/com/srcclr/Main.java
+++ src/main/java/com/srcclr/Main.java
@@ -2,9 +2,6 @@ package com.srcclr;
 
 import org.mindrot.jbcrypt.BCrypt;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-
 public class Main {
 
   public static void main(String[] args) {
@@ -12,5 +9,7 @@ public class Main {
     String hashed = BCrypt.hashpw(candidate, BCrypt.gensalt(12));
 
     BCrypt.checkpw(candidate, hashed);
+
+    BCrypt.checkpw("harmless", hashed);
   }
 }
